ef0e098a86760e55d70ba2ce60ac18328d6966c4
leela <rajleelavathi@gmail.com>
2023-01-16 17:22:19 +0530

    first commit

---
+++ src/main/java/com/example/demose/DemoseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demose/DemoseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
